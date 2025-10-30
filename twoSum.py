e8189cda041df555b4ba01e10f465e15aae9e4a5
Mustafa Bookwala <137228516+bookwalamustafa@users.noreply.github.com>
2025-10-30 01:35:03 -0400

    O(n) solution

--- twoSum.py
+++ twoSum.py
@@ -1,8 +1,20 @@
-def two_sum(nums, target):
-    num_to_index = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_to_index:
-            return [num_to_index[complement], i]
-        num_to_index[num] = i
-    return []
+# First Solution
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+        return []
+
+# Second Solution (Optimized Solution)
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(0, len(nums)):
+            complement = target - nums[i]
+            if nums[i] in seen:
+                return (seen[nums[i]], i)
+            seen[complement] = i
